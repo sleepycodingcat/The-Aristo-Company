9c440fcbcc6f497bb8bf25e0a901edfa13d3b9d3
Rachell Lee <leer17@kgv.hk>
2025-05-01 11:31:30 +0800

    removed footer from index.js

--- index.js
+++ index.js
@@ -41,19 +41,23 @@ function fn(e) {
   // LOGO MOVES ON HOVER
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Load the footer
-    fetch('footer.html')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Failed to load footer: ${response.statusText}`);
-        }
-        return response.text();
-      })
-      .then(data => {
-        console.log(data); // Log the fetched content
-  
-        document.getElementById('footer-placeholder').innerHTML = data;
-      })
+    const logoContainer = document.querySelector('#logoContainer');
+    const video = document.querySelector('#logo');
+    function playVideo() {
+        video.play();
+    }
+
+    function pauseVideo() {
+        video.pause();
+        video.currentTime = 0;
+    }
+
+    logoContainer.addEventListener('mouseenter', playVideo);
+    logoContainer.addEventListener('mouseleave', pauseVideo);
+
+    // Add touch support for mobile devices
+    logoContainer.addEventListener('touchstart', playVideo);
+    logoContainer.addEventListener('touchend', pauseVideo);
     });
   
    
